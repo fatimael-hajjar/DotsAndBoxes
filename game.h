9e7d934b29622657d7422c9b85f8757678106ecb
fatimael-hajjar <fatimasaidelh@gmail.com>
2026-04-19 18:48:57 +0300

    game.h

--- game.h
+++ game.h
@@ -4,22 +4,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Grid dimensions*/
+/* Grid dimensions */
 #define ROWS 5
 #define COLS 6
 
-/*Function prototypes*/
+/* Function prototypes */
 
 // Initializes the game board with empty
 void initBoard();
 
-//Displays the board currently
+// Displays the board currently
 void printBoard();
 
-//Draw line between two points chosen by the player and returns 1 if works, 0 if not
+// Draw line between two points chosen by the player and returns 1 if works, 0 if not
 int addLine(int r1, int c1, int r2, int c2, char player);
 
-//Checks if the last move completed any boxes, returns the numbers of boxes completed to update the score
+// Checks if the last move completed any boxes, returns the numbers of boxes completed to update the score
 int checkForBoxes(char player);
+
+extern int h[5][5];
+extern int v[4][6];
+extern char boxes[4][5];
 
 #endif
